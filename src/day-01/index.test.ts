bfeb9b5a6480cf395b06d3e5dc00c1280681b5f0
mackie <macklinu@users.noreply.github.com>
2021-12-01 11:06:25 -0500

    Solve Day 1

--- src/day-01/index.test.ts
+++ src/day-01/index.test.ts
@@ -1,5 +1,39 @@
-import { placeholder } from '.'
+import { createIncreaseCounter } from '.'
+import * as fs from 'fs'
+import * as path from 'path'
 
-test('placeholder()', () => {
-  expect(placeholder()).toEqual('placeholder')
+const sampleInput = () =>
+  fs.promises.readFile(path.resolve(__dirname, 'sampleInput.txt'), 'utf-8')
+
+const puzzleInput = () =>
+  fs.promises.readFile(path.resolve(__dirname, 'puzzleInput.txt'), 'utf-8')
+
+const totalIncreases = createIncreaseCounter(
+  ({ currentValue, values, index }) => {
+    const previousValue = values[index - 1]
+    return currentValue > previousValue
+  }
+)
+
+const totalIncreasesWindow = createIncreaseCounter(({ values, index }) => {
+  const sum = (array: number[]): number => array.reduce((a, b) => a + b, 0)
+  const previousValues = [values[index - 1], values[index], values[index + 1]]
+  const currentValues = [values[index], values[index + 1], values[index + 2]]
+  return sum(currentValues) > sum(previousValues)
+})
+
+test('solves part 1 sample input', async () => {
+  expect(totalIncreases(await sampleInput())).toEqual(7)
+})
+
+test('solves part 1 puzzle input', async () => {
+  expect(totalIncreases(await puzzleInput())).toEqual(1655)
+})
+
+test('solves part 2 sample input', async () => {
+  expect(totalIncreasesWindow(await sampleInput())).toEqual(5)
+})
+
+test('solves part 2 puzzle input', async () => {
+  expect(totalIncreasesWindow(await puzzleInput())).toEqual(1683)
 })
--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -1,3 +1,23 @@
-export function placeholder(): string {
-  return 'placeholder'
+export function createIncreaseCounter(
+  comparisonFunction: (comparison: {
+    currentValue: number
+    values: number[]
+    index: number
+  }) => boolean
+) {
+  return (input: string): number => {
+    const values = input.split('\n').filter(Boolean).map(Number)
+
+    return values.reduce((increaseCount, value, index) => {
+      if (index === 0) {
+        return increaseCount
+      }
+
+      if (comparisonFunction({ currentValue: value, values, index })) {
+        return increaseCount + 1
+      }
+
+      return increaseCount
+    }, 0)
+  }
 }
